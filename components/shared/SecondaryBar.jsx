8c8269db0edf3fe4146977300aab296143bc874a
Abdullah Al Rajin <adcldeveloper@gmail.com>
2024-12-15 11:40:50 +0600

    updated secondary navbar to show  login icon instead of rtz logo

--- components/shared/SecondaryBar.jsx
+++ components/shared/SecondaryBar.jsx
@@ -1,7 +1,8 @@
 'use client';
-import { FaPhoneAlt, FaUserCircle } from "react-icons/fa";
+
+import React from 'react';
 import { MdEmail, MdLogin } from "react-icons/md";
-import { motion } from "framer-motion";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const SecondaryBar = () => {
   const containerVariants = {
@@ -15,68 +16,52 @@ const SecondaryBar = () => {
   };
 
   return (
-    <motion.div
-      className="bg-white text-black"
-      initial="hidden"
-      animate="visible"
-      variants={containerVariants}
-    >
+    <div className="bg-white text-black">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between py-3">
-          <motion.div
-            className="flex items-center space-x-6"
-            variants={itemVariants}
-          >
-            <motion.a
+          <div className="flex items-center space-x-6">
+            <a
               href="tel:01886349919"
               className="flex items-center text-black hover:text-[#A93226] transition duration-300"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
             >
               <FaPhoneAlt className="mr-2" />
               <span className="text-sm font-semibold hidden lg:flex">01974955226</span>
-            </motion.a>
-            <motion.a
+            </a>
+            <a
               href="mailto:dilal328@gmail.com"
               className="flex items-center text-black hover:text-[#A93226] transition duration-300"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
             >
               <MdEmail className="mr-2" />
               <span className="text-sm font-semibold hidden lg:flex">contact@cubspdc.com</span>
-            </motion.a>
-          </motion.div>
-          <motion.div
-            className="flex items-center justify-end"
-            variants={itemVariants}
-          >
-            <motion.a
-              href="/"
-              className="flex items-center text-black hover:text-[#A93226] transition duration-300"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
+            </a>
+          </div>
+          <div className="flex items-center justify-end">
+            {/* Mobile Login Icon */}
+            <a
+              href="/login"
+              className="lg:hidden flex items-center text-black hover:text-[#A93226] transition duration-300"
             >
-              <img src="/rtz-new.png" alt="Logo" className="w-20 lg:hidden" />
-            </motion.a>
-            <motion.a
+              <MdLogin className="text-2xl" />
+            </a>
+            
+            {/* Desktop Login Button */}
+            <a
               href="/login"
               className="bg-black text-white font-semibold py-2 px-4 rounded-full shadow-md hover:bg-slate-700 transition duration-300 hidden lg:flex"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
             >
               <button className="flex text-md font-semibold text-white">
                 <span className="text-xl">
                   <MdLogin />
                 </span>
                 <span className="px-1">
-                  Login{" "}
+                  Login
                 </span>
               </button>
-            </motion.a>
-          </motion.div>
+            </a>
+          </div>
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
